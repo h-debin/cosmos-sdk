db961152e047b3f304acf7e2d896ec968aae3dfc
rigelrozanski <rigel.rozanski@gmail.com>
2019-03-13 16:14:22 -0400

    consistent command in help and pull request template

--- cmd/sdkch/main.go
+++ cmd/sdkch/main.go
@@ -287,11 +287,10 @@ func printUsage() {
 Maintain unreleased changelog entries in a modular fashion.
 
 Commands:
-    add section stanza [message]    Add an entry file.
-                                    If message is empty, start the editor to edit
-                                    the message.
-    generate [version]              Generate a changelog in Markdown format and print
-                                    it to STDOUT. version defaults to UNRELEASED.
+    add [section] [stanza] [message]  Add an entry file. If message is empty, start 
+	                                  the editor to edit the message.
+    generate [version]                Generate a changelog in Markdown format and print
+                                      it to STDOUT. version defaults to UNRELEASED.
 
     Sections             Stanzas
          ---                 ---
